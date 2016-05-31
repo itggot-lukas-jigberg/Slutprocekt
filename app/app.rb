44274ceec9d516c4316d910eeea31d0927f1f38d
Lukas Jigberg <lukas.jigberg@itggot.se>
2016-05-31 18:55:55 +0200

    Design & Fortsatt utveckling
    Jag har fortsatt att arbeta vidare med hur designen skall vara och hur
    servern skall användas. Nu har jag dock bestämt mig för att ändra på en
    hel del grejer för att hinna med allt. Därför valde jag att göra nya
    mockups, för några nya grejor, sammtidigt som jag "tar bort" gamla.

--- app/app.rb
+++ app/app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
     if accessible.include?(request.path_info)
 
       if session[:user_id]
-        redirect '/user'
+        redirect '/'
       end
 
     else
@@ -84,7 +84,7 @@ class App < Sinatra::Base
       if params["password"] == params["password_repeat"]
         user = User.create(username: params["username"], mail: params["email"], password: params["password"])
         if User.get(user.id)
-          session[:user_id] = user.id
+          #session[:user_id] = user.id
           redirect '/login'
         end
         redirect '/sign-up'
@@ -93,13 +93,22 @@ class App < Sinatra::Base
   end
 
   post '/create_note' do
+    time_stamp = DateTime.parse(params["time"]) rescue time_stamp = nil
     note = Note.create(titel: params["titel"], info: params["information"], time_stamp: params["time"], user_id: @user.id)
-    redirect "/note/#{note.id}"
+    if Note.get(note.id)
+      redirect "/note/#{note.id}"
+    else
+      redirect back
+    end
   end
 
   get '/note/:note_id' do |note_id|
-    @note =
-    erb :note
+    @note = Note.get(note_id)
+    if @note.user == @user
+      erb :note
+    else
+      redirect back
+    end
   end
 
   post '/logout' do
--- app/db/seed.rb
+++ app/db/seed.rb
@@ -12,6 +12,8 @@ class Seeder
   def self.notes
     Note.create(titel: 'Köpa Kazoo', info: 'ville tycker att han ska köpa en kazo på torget', time_stamp: '2016,7,4',user_id: 1)
     Note.create(titel: 'ÄtaMat', info: 'Gå och äta', time_stamp: '2001,3,16', user_id: 1)
+    Note.create(titel: 'Buggfix', info: 'När man isg uppar så directas man till / istället för att koden på app som gör att man inte kan fuska sig in funkar för linkar också', time_stamp: '2016,5,31', user_id: 1)
+    Note.create(titel: 'Betygsunderlag', info: 'Fråga Daniel var man hittar dem', time_stamp: '2016,5,31', user_id: 1)
   end
 
   def self.datetime
